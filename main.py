6269c2b82f3f4490c38a8e9801f8c251d8a7b3b7
parishilan <parishilanrayamajhi@gmail.com>
2022-08-11 16:53:36 +0545

    class based implementatino

--- main.py
+++ main.py
@@ -8,71 +8,74 @@ from ast import literal_eval
 from common_utils.helpers import common_ut as common_util, custom_exception
 import common_utils.logger
 
-log_dir = "/var/log/bitgo"
+log_dir = "/var/log/bitgo" ## all logs recide on this dir, so need to create this dir first
 log_category="script"
 
 logger = common_utils.logger.MyLogger(log_dir, log_category)
 
+class TranxAncestor:
 
-base_url = "https://blockstream.info/api/"
-block_hash="000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732"
- 
+    def __init__(self,block_hash=None):
+        self.block = 680000
+        self.block_hash = block_hash
+        self.base_url = "https://blockstream.info/api/"
 
-def get_total_tx_count(block_hash):
-    '''
-    calculate the total no of transcaction in the given block
-    '''
-    try:
-        url = base_url+'block/{}'.format(block_hash)
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = json.loads(response.content.decode('UTF-8')).get("tx_count",0)
-            return(data)
+    def get_total_tx_count(self):
+        '''
+        calculate the total no of transcaction in the given block
+        '''
+        try:
+            url = self.base_url+'block/{}'.format(self.block_hash)
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = json.loads(response.content.decode('UTF-8')).get("tx_count",0)
+                return(data)
 
-    except Exception as e:
-        error = common_util.get_error_traceback(sys, e)
-        logger.error_logger(error)
-        raise
-
-
-def get_all_txids(block_hash):
-    '''
-    return list of total transaction in given block
-    '''
-
-    try:
-        url = base_url+'block/{}/txids'.format(block_hash)
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = literal_eval(response.content.decode('UTF-8'))
-            return data
-
-    except Exception as e:
-        error = common_util.get_error_traceback(sys, e)
-        logger.error_logger(error)
-        raise
+        except Exception as e:
+            error = common_util.get_error_traceback(sys, e)
+            logger.error_logger(error)
+            raise
 
 
-def get_raw_trnx(index_count):
-    """
-    Returns transaction information given block:
-    """
-    try:
-        url = base_url+'block/{}/txs/{}'.format(block_hash,index_count)
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = json.loads(response.content.decode('UTF-8'))
-            return data
-    except Exception as e:
-        error = common_util.get_error_traceback(sys, e)
-        logger.error_logger(error)
-        raise
+    def get_all_txids(self):
+        '''
+        return list of total transaction in given block
+        '''
+
+        try:
+            url = self.base_url+'block/{}/txids'.format(self.block_hash)
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = literal_eval(response.content.decode('UTF-8'))
+                return data
+
+        except Exception as e:
+            error = common_util.get_error_traceback(sys, e)
+            logger.error_logger(error)
+            raise
+
+    @staticmethod
+    def get_raw_trnx(block_hash,index_count):
+        """
+        Returns transaction information given block:
+        """
+        try:
+            base_url = 'https://blockstream.info/api/'
+            url = base_url+'block/{}/txs/{}'.format(block_hash,index_count)
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = json.loads(response.content.decode('UTF-8'))
+                return data
+        except Exception as e:
+            error = common_util.get_error_traceback(sys, e)
+            logger.error_logger(error)
+            raise
 
 
-    except Exception as e:
-        error = common_util.get_error_traceback(sys, e)
-        logger.error_logger(error)
-        raise
+        except Exception as e:
+            error = common_util.get_error_traceback(sys, e)
+            logger.error_logger(error)
+            raise
 
 
 def main():
@@ -81,12 +84,13 @@ def main():
     :return:
     """
     try:
+
         block_number = 680000
         all_txids_in_block = get_all_txids(block_hash) # returns array 
         total_txns = int(get_total_tx_count(block_hash))
         total_index = total_txns/int(25) # 25 is the rate limit as per the api
         index_count = 0
-        final_count = 0
+        ancenstry_count = 0
         while (index_count <= total_txns):
             raw_tx = get_raw_trnx(index_count)
             if raw_tx:
@@ -97,11 +101,11 @@ def main():
                         vout_int=temp_data["vout"]
                         if raw_tx1 in all_txids_in_block:
                             # Ancestry found
-                            final_count = final_count+1
+                            ancenstry_count = ancenstry_count+1
                 index_count += 25
 
         
-        print ("Total count  is :",final_count)   #final_count = 389
+        print ("Total count  is :",ancenstry_count)   #ancenstry_count = 389
         raise
 
     except Exception as e:
@@ -112,4 +116,27 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    bitgoExample = TranxAncestor("000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732") #pre calculate the block hash
+
+    all_txids_in_block = bitgoExample.get_all_txids()
+    total_txns = int(bitgoExample.get_total_tx_count())
+    total_index = total_txns/int(25) # 25 is the rate limit as per the api
+    index_count = 0
+    ancenstry_count = 0
+    while (index_count <= total_txns):
+        raw_tx = bitgoExample.get_raw_trnx(bitgoExample.block_hash,index_count)
+        
+        if raw_tx:
+            for single_tx_data in raw_tx:
+                vin = single_tx_data["vin"]
+                for temp_data in vin:
+                    raw_tx1=temp_data["txid"]
+                    vout_int=temp_data["vout"]
+                    if raw_tx1 in all_txids_in_block:
+                        # Ancestry found
+                        ancenstry_count = ancenstry_count+1
+        index_count += 25
+    
+    print ("Total count  is :",ancenstry_count)   #ancenstry_count = 389
+    
+
