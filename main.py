064ed9e18f0d6d6f4335a27d228acce9892569c9
parin13 <parishilanrayamajhi@gmail.com>
2022-08-15 00:40:48 +0545

    optimizations underway: main func seperated

--- main.py
+++ main.py
@@ -1,57 +1,75 @@
-import sys,os
-from os.path import dirname, join, abspath
+import sys, time
 from heapq import heappop, heappush, heapify
-import time
-import json
-from ast import literal_eval
+from btc_block import logger, BtcBlock
+from common_utils.helpers import common_ut as common_util
+import ref_string
 
-from trnxAncestor import TranxAncestor 
-from common_utils.helpers import common_ut as common_util, custom_exception 
-import common_utils.logger
-
-logger = common_utils.logger.MyLogger(directory="/var/log/bitgo", category="script")
-
-heap =[]
+heap = []
 heapify(heap)
+all_txids_in_block = []
 
-if __name__ == '__main__':
+def calculate_anscestor(single_tx_data):
+    """
+    recursive function to calculate total number of ancestors
+    """
     try:
+        global all_txids_in_block
+        vin = single_tx_data["vin"]
+        for vin_data in vin:
+            raw_tx1=vin_data["txid"]
+            if raw_tx1 in  all_txids_in_block:
+                calculate_anscestor.ancient_count += 1
+                calculate_anscestor(BtcBlock.get_single_tx_detail(raw_tx1))
+            return 0
+    except Exception as e:
+        error = common_util.get_error_traceback(sys, e)
+        logger.error_logger(error)
+        raise
 
-        start_time = time.perf_counter()
+ 
+def main():
+    try:
+        global all_txids_in_block
 
-        bitgoExample = TranxAncestor(block_hight=680000)
+        bitgoExample =  BtcBlock(block_hight=ref_string.EnvVariables.crawl_block_height)
         bitgoExample.get_block_hash()
-        all_txids_in_block = bitgoExample.get_all_txids()
+        all_txids_in_block = bitgoExample.get_all_txids_in_block()
         total_txns = int(bitgoExample.get_total_tx_count())
-        total_index = total_txns/int(25) # 25 is the rate limit as per the api  # TODO ip from commadn 
         index_count = 0
 
         while (index_count <= total_txns):
             raw_tx = bitgoExample.get_raw_trnx(index_count)
             
-            if raw_tx:   ## why not if not 
+            if raw_tx:  
                 for single_tx_data in raw_tx:
                     parent_tx = single_tx_data.get('txid')
                     vin = single_tx_data.get("vin", [])
 
-                    # whyt not recurssin here it self
-                    for temp_data in vin:  # use prioper variables
-                        raw_tx1=temp_data["txid"]
-                        vout_int=temp_data["vout"]
+                    for vin_data in vin: 
+                        raw_tx1=vin_data["txid"]
                         if raw_tx1 in all_txids_in_block:
                             # Ancestry found
-                            bitgoExample.calculate_ans.ancient_count=1  
-                            bitgoExample.calculate_ans(bitgoExample.get_single_tx_detail(raw_tx1)) #calculate all parents recirsivly
-                            heappush(heap, (-1 * (bitgoExample.calculate_ans.ancient_count),parent_tx))
+                            calculate_anscestor.ancient_count=1  
+                            calculate_anscestor(bitgoExample.get_single_tx_detail(raw_tx1)) #calculate all parents recirsivly
+                            heappush(heap, (-1 * (calculate_anscestor.ancient_count),parent_tx))
 
-            index_count += 25 # single point of truth
+            index_count += ref_string.EnvVariables.api_trnx_detail_limit
         
-        c = 0
-        while not c>10:
-            ans = heappop(heap)
-            print ("Ancestr Count : {} , txid: {}".format(int(ans[0]*(-1)),ans[1]) )
-            c += 1
+        output_count = 0
+        while output_count > ref_string.EnvVariables.limit_to_show_op:
+            ancestor_family = heappop(heap)
+            print ("Ancestr Count : {} , txid: {}".format(int(ancestor_family[0]*(-1)),ancestor_family[1]) )
+            output_count += 1
 
+    except Exception as e:
+        error = common_util.get_error_traceback(sys, e)
+        logger.error_logger(error)
+        raise
+
+if __name__ == '__main__':
+    try:
+        start_time = time.perf_counter()
+        main()
         end_time = time.perf_counter()
         print("Program Execution Time {} seconds".format(end_time - start_time))
         print("End oF the Code")
@@ -59,4 +77,3 @@ if __name__ == '__main__':
     except Exception as e:
         error = common_util.get_error_traceback(sys, e)
         logger.error_logger(error)
-        raise
--- ref_string.py
+++ ref_string.py
@@ -3,3 +3,4 @@ class EnvVariables():
     crawl_block_height=680000
     base_url="https://blockstream.info/api/"
     api_trnx_detail_limit=25
+    limit_to_show_op=10
